cef5c368023914850c23b8f7cb075d1e42ae486a
mahendrayudha <yudhaoctavian01@gmail.com>
2020-02-26 19:06:16 +0700

    DONE

--- E41180097_OctavianYudhaMahendra/belajar_ci/application/config/config.php
+++ E41180097_OctavianYudhaMahendra/belajar_ci/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.0.1/belajar_ci/';
+$config['base_url'] = 'http://127.0.0.1/NauraFarm_Smt4/E41180097_OctavianYudhaMahendra/belajar_ci/';
 
 /*
 |--------------------------------------------------------------------------
