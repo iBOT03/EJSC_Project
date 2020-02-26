04f18f6de94a43698ec6ec28a45734487b8e739e
Andrea Santana Adzani <45676944+iBOT03@users.noreply.github.com>
2020-02-26 14:44:31 +0700

    myFolder

---
+++ E41180362_AndreaSantanaAdani/belajar-ci/application/views/HomeView.php
@@ -0,0 +1,7 @@
+<h1>Selamat Datang di Website CI Pertama Saya (Percobaan HomeView)</h1>
+<p>Berikut ini Mahasiswa Yang Membuat: </p>
+
+Nama : <?php echo $data["nama"]; ?> <br>
+Status : <?php echo $data["status"]; ?> <br>
+Website : <?php echo $data["website"]; ?> <br>
+
