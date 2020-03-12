73f539af9773a9ab8003dca2d12a87392331f2bb
Andrea Santana Adzani <45676944+iBOT03@users.noreply.github.com>
2020-03-12 11:27:53 +0700

    up

---
+++ EJSC_Project/admin/application/views/admin/notfound.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <!-- Head -->
+  <?php $this->load->view("admin/_partials/head.php") ?>
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <?php $this->load->view("admin/_partials/sidebar.php") ?>
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <?php $this->load->view("admin/_partials/topbar.php") ?>
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- 404 Error Text -->
+          <div class="text-center">
+            <div class="error mx-auto" data-text="404">404</div>
+            <p class="lead text-gray-800 mb-5">Page Not Found</p>
+            <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
+            <a href="<?php echo base_url('index.php/admin/dashboard') ?>">&larr; Back to Dashboard</a>
+          </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <?php $this->load->view("admin/_partials/footer.php") ?>
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <?php $this->load->view("admin/_partials/scrolltop.php") ?>
+
+  <!-- Logout Modal-->
+  <?php $this->load->view("admin/_partials/modal.php") ?>
+
+  <!-- JavaScript-->
+  <?php $this->load->view("admin/_partials/js.php") ?>
+
+</body>
+
+</html>
