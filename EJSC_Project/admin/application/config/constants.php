8d344826b4b6bb68d426a0db6eedcb0cf5861f2a
mahendrayudha <yudhaoctavian01@gmail.com>
2020-03-12 15:35:27 +0700

    update panel admin

--- EJSC_Project/admin/application/config/constants.php
+++ EJSC_Project/admin/application/config/constants.php
@@ -91,4 +91,4 @@ defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest auto
 |
 */
 
-define('SITE_NAME', 'Bima Technologies');
+define('SITE_NAME', 'East Java Super Corridor');
