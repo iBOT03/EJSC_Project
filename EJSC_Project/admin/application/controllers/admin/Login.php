8d917eb61d19e40ce128942bf1955b8bf521ac67
Andrea Santana Adzani <45676944+iBOT03@users.noreply.github.com>
2020-03-15 21:54:00 +0700

    Update Login.php

--- EJSC_Project/admin/application/controllers/admin/Login.php
+++ EJSC_Project/admin/application/controllers/admin/Login.php
@@ -7,7 +7,7 @@ class Login extends CI_Controller {
 	}
 
 	public function index() {
-		$this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[6]');
+		$this->form_validation->set_rules('password', 'Password', 'required|trim|min_length[6]|max_length[20]');
 		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
 		
 		if($this->form_validation->run() == false){
