83880b573411fb07518a32f852dcb1c25545f3d3
Andrea Santana Adzani <45676944+iBOT03@users.noreply.github.com>
2020-03-15 11:27:10 +0700

    Update tambahpengguna.php

--- EJSC_Project/admin/application/views/admin/tambahpengguna.php
+++ EJSC_Project/admin/application/views/admin/tambahpengguna.php
@@ -30,7 +30,7 @@
 
                     <!-- Page Heading -->
                     <form method="post">
-                        <h1 class="h3 mb-2 text-gray-800">Tambah Data Administrator</h1>
+                        <h1 class="h3 mb-2 text-gray-800">Tambah Data Pengguna</h1>
 
                         <div class="card shadow mb-4">
                             <div class="card-body">
