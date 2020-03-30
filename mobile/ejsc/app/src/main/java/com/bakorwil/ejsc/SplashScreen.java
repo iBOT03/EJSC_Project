d1aa217acefffa86fdc161fdbc8fb8700f260c9b
mahendrayudha <yudhaoctavian01@gmail.com>
2020-03-30 15:01:21 +0700

    task yudha

--- mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/SplashScreen.java
+++ mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/SplashScreen.java
@@ -2,13 +2,36 @@ package com.bakorwil.ejsc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.bakorwil.ejsc.akun.LoginActivity;
+
+import static java.lang.Thread.sleep;
 
 public class SplashScreen extends AppCompatActivity {
+    private int time = 1000;
+    String profil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    SplashScreen.this.finish();
+                    Intent b = new Intent(SplashScreen.this, LoginActivity.class);
+                    startActivity(b);
+                } catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                }
+            }
+        }, time);
     }
 }
--- mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/botnav/akun/AkunFragment.java
+++ mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/botnav/akun/AkunFragment.java
@@ -14,7 +14,7 @@ public class AkunFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_event, container, false);
+        View root = inflater.inflate(R.layout.fragment_akun, container, false);
         return root;
     }
 }
