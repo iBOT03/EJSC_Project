7fd25719a30bc7328fc111c2c575e9d91b56f88a
mahendrayudha <yudhaoctavian01@gmail.com>
2020-04-02 13:31:25 +0700

    XML notifikasi+event DONE

--- mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/botnav/beranda/BerandaFragment.java
+++ mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/botnav/beranda/BerandaFragment.java
@@ -1,11 +1,18 @@
 package com.bakorwil.ejsc.botnav.beranda;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.bakorwil.ejsc.R;
@@ -14,7 +21,21 @@ public class BerandaFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_beranda, container, false);
-        return root;
+        View view = inflater.inflate(R.layout.fragment_beranda, container, false);
+
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.menu);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.btnNotifikasi) {
+                    Intent notifikasi = new Intent(getActivity(), NotifikasiActivity.class);
+                    startActivity(notifikasi);
+                }
+                return false;
+            }
+        });
+        return view;
     }
 }
