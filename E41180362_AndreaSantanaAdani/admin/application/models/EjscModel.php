583f6528c9bb5d2c085bfffa620cc78d70a4a091
Andrea Santana Adzani <45676944+iBOT03@users.noreply.github.com>
2020-04-09 11:09:05 +0700

    update Dashboard

--- E41180362_AndreaSantanaAdani/admin/application/models/EjscModel.php
+++ E41180362_AndreaSantanaAdani/admin/application/models/EjscModel.php
@@ -1,75 +1,118 @@
 <?php
-class EjscModel extends CI_Model {
-    public function detail($nik){
+class EjscModel extends CI_Model
+{
+    public function detail($nik)
+    {
         $this->load->database();
         $this->db->where('NIK', $nik);
         return $this->db->get("akun")->result();
     }
     //GET ADMIN DATA
-    public function getadmin() {
+    public function getadmin()
+    {
         $this->load->database();
         return $this->db->get_where("akun", ['level' => 1])->result();
     }
     //INSERT ADMIN DATA
-    public function tambahadmin($data = array()) {
+    public function tambahadmin($data = array())
+    {
         $this->load->database();
         return $this->db->insert("akun", $data);
     }
     //INSERT USER DATA
-    public function tambahpengguna($data = array()){
+    public function tambahpengguna($data = array())
+    {
         $this->load->database();
         return $this->db->insert("akun", $data);
     }
     //GET USER DATA
-    public function getuser() {
+    public function getuser()
+    {
         $this->load->database();
         return $this->db->get_where("akun", ['level' => 2])->result();
     }
     //DELETE ADMIN DATA
-    public function hapusadmin($nik) {
+    public function hapusadmin($nik)
+    {
         $this->load->database();
         $this->db->where('NIK', $nik);
         return $this->db->delete("akun");
     }
     //DELETE USER DATA
-    public function hapususer($nik) {
+    public function hapususer($nik)
+    {
         $this->load->database();
         $this->db->where('NIK', $nik);
         return $this->db->delete("akun");
     }
     //UPDATE ADMIN DATA
-    public function ubahadmin($data = array(), $nik) {
+    public function ubahadmin($data = array(), $nik)
+    {
         $this->load->database();
         $this->db->where('NIK', $nik);
         return $this->db->update("akun", $data);
     }
     //UPDATE USER DATA
-    public function ubahuser($data = array(), $nik) {
+    public function ubahuser($data = array(), $nik)
+    {
         $this->load->database();
         $this->db->where('NIK', $nik);
         return $this->db->update("akun", $data);
     }
-    public  function jeniskomunitas(){
+    public  function jeniskomunitas()
+    {
         $query = $this->db->get('komunitas');
         return $query->result_array();
     }
     //GET KATEGORI KOMUNITAS
-    public function kategori(){
+    public function kategori()
+    {
         $this->load->database();
         return $this->db->get("kategori_komunitas")->result();
     }
 
     //GET EVENT DATA
-    public function getevent() {
+    public function getevent()
+    {
         $this->load->database();
         return $this->db->get("event")->result();
     }
 
     //GET DETAIL EVENT
-    public function detail_event($id){
+    public function detail_event($id)
+    {
         $this->load->database();
         $this->db->where('ID_EVENT', $id);
         return $this->db->get("event")->result();
     }
-
+    //GET TOTAL PENGGUNA
+    public function pengguna()
+    {
+        $query = $this->db->get_where("akun", ['level' => 2]);
+        if ($query->num_rows() > 0) {
+            return $query->num_rows();
+        } else {
+            return 0;
+        }
+    }
+    //GET TOTAL BOKING
+    public function boking()
+    {
+        $query = $this->db->get("booking");
+        if ($query->num_rows() > 0) {
+            return $query->num_rows();
+        } else {
+            return 0;
+        }
+    }
+    //GET TOTAL EVENT
+    public function event()
+    {
+        $query = $this->db->get("event");
+        if ($query->num_rows() > 0) {
+            return $query->num_rows();
+        } else {
+            return 0;
+        }
+    }
 }
--- E41180362_AndreaSantanaAdani/admin/application/views/admin/dashboard.php
+++ E41180362_AndreaSantanaAdani/admin/application/views/admin/dashboard.php
@@ -3,174 +3,182 @@
 
 <head>
 
-  <!-- Head -->
-  <?php $this->load->view("admin/_partials/head.php") ?>
+    <!-- Head -->
+    <?php $this->load->view("admin/_partials/head.php") ?>
 
 </head>
 
 <body id="page-top">
 
-  <!-- Page Wrapper -->
-  <div id="wrapper">
+    <!-- Page Wrapper -->
+    <div id="wrapper">
 
-    <!-- Sidebar -->
-    <?php $this->load->view("admin/_partials/sidebar.php") ?>
+        <!-- Sidebar -->
+        <?php $this->load->view("admin/_partials/sidebar.php") ?>
 
-    <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column">
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
 
-      <!-- Main Content -->
-      <div id="content">
+            <!-- Main Content -->
+            <div id="content">
 
-        <!-- Topbar -->
-        <?php $this->load->view("admin/_partials/topbar.php") ?>
+                <!-- Topbar -->
+                <?php $this->load->view("admin/_partials/topbar.php") ?>
 
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
 
-          <!-- Page Heading -->
-          <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn btn-info btn-icon-split shadow-sm">
-              <span class="icon text-white-50">
-                <i class="fas fa-download"></i>
-              </span>
-              <span class="text"> Download Laporan</span>
-            </a>
-          </div>
-
-          <!-- Content Row -->
-          <div class="row">
-
-            <!-- Total Kunjungan Card -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Kunjungan (Booking)</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">999</div>
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn btn-info btn-icon-split shadow-sm">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-download"></i>
+                            </span>
+                            <span class="text"> Download Laporan</span>
+                        </a>
                     </div>
-                    <div class="col-auto">
-                      <i class="fas fa-users fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <!-- Total Event Card -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Event Terselenggara</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">999</div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-calendar-alt fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+                    <!-- Content Row -->
+                    <div class="row">
 
-            <!-- Total pengguna Card -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Total Pengguna</div>
-                      <div class="row no-gutters align-items-center">
-                        <div class="col-auto">
-                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">999</div>
+                        <!-- Total Kunjungan Card -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total
+                                                Kunjungan (Booking)</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                <?php echo $total_boking; ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-users fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-user fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <!-- Feedback Card -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Feedback</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">999</div>
+                        <!-- Total Event Card -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total
+                                                Event Terselenggara</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                <?php echo $total_event; ?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-calendar-alt fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Total pengguna Card -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-info shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Total
+                                                Pengguna</div>
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col-auto">
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                                        <?php echo $total_pengguna; ?></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-user fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Feedback Card -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-warning shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                Feedback</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">999</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-auto">
-                      <i class="fas fa-comments fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
 
-          <!-- Content Row -->
+                    <!-- Content Row -->
 
-          <div class="row">
+                    <div class="row">
 
-            <!-- Area Chart -->
-            <div class="col-xl-12">
-              <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-info">Grafik Kunjungan</h6>
-                  <!-- <div class="dropdown no-arrow"> -->
-                    <!-- <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <!-- Area Chart -->
+                        <div class="col-xl-12">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-info">Grafik Kunjungan</h6>
+                                    <!-- <div class="dropdown no-arrow"> -->
+                                    <!-- <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                     </a> -->
-                    <!-- <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                                    <!-- <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
                       <div class="dropdown-header">Filter Berdasarkan:</div>
                       <a class="dropdown-item" href="#">Tahun</a>
                       <a class="dropdown-item" href="#">Bulan</a>
                       <a class="dropdown-item" href="#">Minggu</a>
                       <a class="dropdown-item" href="#">Hari</a>
                     </div> -->
-                  <!-- </div> -->
+                                    <!-- </div> -->
+                                </div>
+                                <!-- Card Body -->
+                                <div class="card-body">
+                                    <div class="chart-area">
+                                        <canvas id="grafikPengunjung"></canvas>
+                                    </div>
+                                </div> <!-- End Chart -->
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                  <div class="chart-area">
-                    <canvas id="grafikPengunjung"></canvas>
-                  </div>
-                </div> <!-- End Chart -->
-              </div>
+                <!-- /.container-fluid -->
+
             </div>
-          </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <?php $this->load->view("admin/_partials/footer.php") ?>
 
         </div>
-        <!-- /.container-fluid -->
-
-      </div>
-      <!-- End of Main Content -->
-
-      <!-- Footer -->
-      <?php $this->load->view("admin/_partials/footer.php") ?>
+        <!-- End of Content Wrapper -->
 
     </div>
-    <!-- End of Content Wrapper -->
+    <!-- End of Page Wrapper -->
 
-  </div>
-  <!-- End of Page Wrapper -->
+    <!-- Scroll to Top Button-->
+    <?php $this->load->view("admin/_partials/scrolltop.php") ?>
 
-  <!-- Scroll to Top Button-->
-  <?php $this->load->view("admin/_partials/scrolltop.php") ?>
+    <!-- Logout Modal-->
+    <?php $this->load->view("admin/_partials/modal.php") ?>
 
-  <!-- Logout Modal-->
-  <?php $this->load->view("admin/_partials/modal.php") ?>
-
-  <!-- JavaScript-->
-  <?php $this->load->view("admin/_partials/js.php") ?>
+    <!-- JavaScript-->
+    <?php $this->load->view("admin/_partials/js.php") ?>
 
 </body>
 
