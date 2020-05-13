7cb53ec736d284899d93dd4e55fdb3727457c6fa
Andrea Santana Adzani <45676944+iBOT03@users.noreply.github.com>
2020-05-13 20:26:54 +0700

    Update ServerApi.java

--- mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/configfile/ServerApi.java
+++ mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/configfile/ServerApi.java
@@ -13,13 +13,4 @@ public class ServerApi {
     // Rest API Ruangan
     public static final String URL_RUANGAN = "http://192.168.43.87/EJSC_Project/admin/api/ruangan/ruangan";
 
-    public static final String URL_LOGIN = "http://192.168.43.87/EJSC_Project/admin/api/akun/login";
-    public static final String URL_LUPAPW = "http://192.168.43.87/EJSC_Project/admin/api/akun/lupa";
-    public static final String URL_DAFTAR = "http://192.168.43.87/EJSC_Project/admin/api/akun/register";
-    // Rest API Event
-    public static final String URL_GET_EVENT = "http://192.168.43.87/EJSC_Project/admin/api/event";
-    // Rest API Kontak Kami
-    public static final String URL_KONTAK = "http://192.168.43.87/EJSC_Project/admin/api/kontak/kontak";
-    // Rest API Ruangan
-    public static final String URL_RUANGAN = "http://192.168.43.87/EJSC_Project/admin/api/ruangan/ruangan";
 }
