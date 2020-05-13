e790c5f615ac2f8b71856101d6996245c44b0c28
MaulidyaPriswanti <lidiapriswanti2000@gmail.com>
2020-05-13 23:33:37 +0700

    Akun

--- mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/botnav/akun/AkunFragment.java
+++ mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/botnav/akun/AkunFragment.java
@@ -1,37 +1,51 @@
 package com.bakorwil.ejsc.botnav.akun;
 
-import android.app.Dialog;
+
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+
 import com.bakorwil.ejsc.DialogKontak;
 import com.bakorwil.ejsc.R;
 import com.bakorwil.ejsc.akun.EditAkunActivity;
 import com.bakorwil.ejsc.akun.LoginActivity;
 
+import org.json.JSONArray;
+
+import static android.content.Context.MODE_PRIVATE;
+
+
 public class AkunFragment extends Fragment {
     TextView editAkun, txt_nama, txt_email, keluar, kontakkami;
+    ProgressDialog progressDialog;
+    JSONArray arr;
+    String cmail, cnama;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_akun, container, false);
 
+        bacaPreferensi();
         txt_nama = view.findViewById(R.id.txt_nama);
+        txt_nama.setText(cnama);
         txt_email = view.findViewById(R.id.txt_email);
+        txt_email.setText(cmail);
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
         editAkun = view.findViewById(R.id.txt_edit_akun);
         editAkun.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,4 +73,11 @@ public class AkunFragment extends Fragment {
             }
         });
     }
+
+    private void bacaPreferensi() {
+        SharedPreferences pref = getActivity().getSharedPreferences("akun", MODE_PRIVATE);
+        cmail = pref.getString("email", "0");
+        cnama = pref.getString("nama", "0");
+    }
 }
+
