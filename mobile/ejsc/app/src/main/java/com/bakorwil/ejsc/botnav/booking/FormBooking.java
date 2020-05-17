62355ead273ab590410c8fca42d74248ead55b19
MaulidyaPriswanti <lidiapriswanti2000@gmail.com>
2020-05-17 22:08:20 +0700

    Lanjutan (xml) Booking

--- mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/botnav/booking/FormBooking.java
+++ mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/botnav/booking/FormBooking.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.bakorwil.ejsc.R;
 
-public class BookingActivity3 extends AppCompatActivity {
+public class FormBooking extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_booking3);
+		setContentView(R.layout.form_booking);
 	}
 }
