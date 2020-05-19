86a83be4d2658abb7aa490689613a75cab70cdd6
Octavian Yudha Mahendra <54369399+mahendrayudha@users.noreply.github.com>
2020-05-19 19:56:01 +0700

    Update ServerApi.java

--- mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/configfile/ServerApi.java
+++ mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/configfile/ServerApi.java
@@ -2,16 +2,28 @@ package com.bakorwil.ejsc.configfile;
 
 public class ServerApi {
     // Rest API Akun
-
-    public static final String URL_LOGIN = "http://192.168.1.9/EJSC_Project/admin/api/akun/login";
-    public static final String URL_LUPAPW = "http://192.168.1.9/EJSC_Project/admin/api/akun/lupa";
-    public static final String URL_DAFTAR = "http://192.168.1.9/EJSC_Project/admin/api/akun/register";
-    public static final String URL_UPDATE = "http://192.168.1.9/EJSC_Project/admin/api/akun/update";
+//    public static final String URL_LOGIN = "http://192.168.1.9/EJSC_Project/admin/api/akun/login";
+//    public static final String URL_LUPAPW = "http://192.168.1.9/EJSC_Project/admin/api/akun/lupa";
+//    public static final String URL_DAFTAR = "http://192.168.1.9/EJSC_Project/admin/api/akun/register";
+//    public static final String URL_UPDATE = "http://192.168.1.9/EJSC_Project/admin/api/akun/update";
     // Rest API Event
-    public static final String URL_GET_EVENT = "http://192.168.1.9/EJSC_Project/admin/api/event";
+//    public static final String URL_GET_EVENT = "http://192.168.1.9/EJSC_Project/admin/api/event";
     // Rest API Kontak Kami
-    public static final String URL_KONTAK = "http://192.168.1.9/EJSC_Project/admin/api/kontak/kontak";
+//    public static final String URL_KONTAK = "http://192.168.1.9/EJSC_Project/admin/api/kontak/kontak";
     // Rest API Ruangan
-    public static final String URL_RUANGAN = "http://192.168.1.9/EJSC_Project/admin/api/ruangan/ruangan";
+//    public static final String URL_RUANGAN = "http://192.168.1.9/EJSC_Project/admin/api/ruangan/ruangan";
+
+
+    // Rest API Akun
+    public static final String URL_LOGIN = "http://naurafarm.polije.com/ejsc/api/akun/login";
+    public static final String URL_LUPAPW = "http://naurafarm.polije.com/ejsc/api/akun/lupa";
+    public static final String URL_DAFTAR = "http://naurafarm.polije.com/ejsc/api/akun/register";
+    public static final String URL_UPDATE = "http://naurafarm.polije.com/ejsc/api/akun/update";
+    // Rest API Event
+    public static final String URL_GET_EVENT = "http://naurafarm.polije.com/ejsc/api/event/event";
+    // Rest API Kontak Kami
+    public static final String URL_KONTAK = "http://naurafarm.polije.com/ejsc/api/kontak/kontak";
+    // Rest API Ruangan
+    public static final String URL_RUANGAN = "http://naurafarm.polije.com/ejsc/api/ruangan/ruangan";
 
 }
