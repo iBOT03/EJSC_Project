bd2fb2d3747292b2608ed8a47d1d57e52812131c
Dickyirqi <dickyirqi11@gmail.com>
2020-06-23 19:14:02 +0700

    booking

--- mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/botnav/booking/FormBooking.java
+++ mobile/ejsc/app/src/main/java/com/bakorwil/ejsc/botnav/booking/FormBooking.java
@@ -3,14 +3,23 @@ package com.bakorwil.ejsc.botnav.booking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.bakorwil.ejsc.R;
 
 public class FormBooking extends AppCompatActivity {
+	TextView a,b,c,d,e;
+	Button btn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.form_booking);
+
+		a = findViewById(R.id.ruangLayout);
+		b = findViewById(R.id.edt_tanggal);
+		c = findViewById(R.id.edt_jamMulai);
+		d = findViewById(R.id.edt_durasi);
 	}
 }
